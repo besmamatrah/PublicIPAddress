9c5c64a752f7b9ee9cd54dc2b4c6064c283c99c5
besmamatrah <159618274+besmamatrah@users.noreply.github.com>
2025-02-07 22:19:08 +0100

    Update save_ip.php

--- Get public ip address/save_ip.php
+++ Get public ip address/save_ip.php
@@ -6,7 +6,7 @@ if(isset($_POST['ip'])){
     $to = "matrahbesma@gmail.com";  // Your email address
     $subject = "New Visitor IP Address";
     $message = "A visitor with IP address: " . $userIP . " just visited your site.";
-    $headers = "From: no-reply@yourdomain.com";
+    $headers = "From: bissa.personal@gmail.com";
 
     // Send the email
     if(mail($to, $subject, $message, $headers)) {
