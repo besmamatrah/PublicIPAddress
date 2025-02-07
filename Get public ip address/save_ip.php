fbb052f215d7308b5c6b50ef435d8cec7b5045e2
besmamatrah <159618274+besmamatrah@users.noreply.github.com>
2025-02-07 22:16:55 +0100

    Add files via upload

---
+++ Get public ip address/save_ip.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_POST['ip'])){
+    $userIP = $_POST['ip'];  // Get the IP from the POST request
+
+    // Send the IP via email (replace with your email address)
+    $to = "matrahbesma@gmail.com";  // Your email address
+    $subject = "New Visitor IP Address";
+    $message = "A visitor with IP address: " . $userIP . " just visited your site.";
+    $headers = "From: no-reply@yourdomain.com";
+
+    // Send the email
+    if(mail($to, $subject, $message, $headers)) {
+        echo "IP address sent to email!";
+    } else {
+        echo "Failed to send email.";
+    }
+}
+?>
